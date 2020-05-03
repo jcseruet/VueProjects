87407122f03b7743f137f56865824240c5a22644
Md Jahangir Alam <jahangir01326cse@yahoo.com>
2020-05-03 22:43:16 +1000

    Form element is wrapped in a container

--- src/components/FirstForm.vue
+++ src/components/FirstForm.vue
@@ -1,10 +1,13 @@
 <template>
+<div class="container">
     <div class="form-group row">
         <label class="col-2" for="Age">Age</label>
         <input class="form-control col-4 leftAlign" id="Age" name="Age" type="text" v-model="age" v-validate="'required|min_value:0'"> 
         <div class="text-danger" v-if="errors.has('Age')">{{errors.first('Age')}}</div>
         <div class="text-warning" v-if="showWarning()">Warning: Age has changed from 36</div>
     </div>
+</div>
+
 </template>
 
 <script>
